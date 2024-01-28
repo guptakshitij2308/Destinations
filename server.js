7779b645924bd5bce14174e361e2bdc5b432260c
Kshitij Gupta <guptakshitij2308@gmail.com>
2024-01-29 00:02:49 +0530

    Connected atlas database using mongoose.

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
-
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<USER>",
+  process.env.DATABASE_USER,
+).replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    // console.log(con.connections);
+    console.log("DB connection established successfully.");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 // console.log(app.get('env')); // by default environment set to 'development' by express
 // console.log(process.env);
