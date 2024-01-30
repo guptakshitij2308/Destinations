ccddbaa578b1cdec835d2d7ef772b8fe2e4d5d6a
Kshitij Gupta <guptakshitij2308@gmail.com>
2024-01-30 17:48:17 +0530

    Implemented crud on tours.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,51 +1,74 @@
-const fs = require("fs");
+const Tour = require("../models/tourModel");
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-);
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-exports.getAllTours = (req, res) => {
-  res
-    .status(200)
-    .json({ status: "success", results: tours.length, data: { tours } });
-};
-
-exports.getTour = (req, res) => {
-  const id = +req.params.id;
-
-  if (id > tours.length - 1) {
-    return res.status(404).json({ status: "fail", message: "Invalid Id" });
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: { tours },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
   }
-
-  const tour = tours.find((tour) => tour.id === id);
-  res.status(200).json({ status: "success", data: { tour } });
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      if (!err)
-        res.status(201).json({ status: "success", data: { tour: newTour } });
-      else
-        res
-          .status(500)
-          .json({ status: "error", message: "Error creating tour." });
-    },
-  );
+exports.getTour = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const tour = await Tour.findById(id);
+
+    res.status(200).json({
+      status: "success",
+      data: { tour },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 };
 
-exports.updateTour = (req, res) => {
-  res
-    .status(500)
-    .json({ status: "error", message: "This route is not yet implemented." });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: { tour: newTour },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: " fail",
+      message: err.message,
+    });
+  }
 };
-exports.deleteTour = (req, res) => {
-  res
-    .status(500)
-    .json({ status: "error", message: "This route is not yet implemented." });
+
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // using this will make the new updated object to be returned from the function
+      runValidators: true,
+    });
+
+    res.status(200).json({ status: "success", data: { tour } });
+  } catch (err) {
+    res.status(404).json({ status: "fail", message: err.message });
+  }
+};
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res
+      .status(204)
+      .json({ status: "success", message: "Tour deleted successfully." });
+  } catch (err) {
+    res.status(404).json({ status: "fail", message: err.message });
+  }
 };
