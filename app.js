ad3b5a069b8d3ff3876eed91a28d829ffd694cce
Kshitij Gupta <guptakshitij2308@gmail.com>
2024-01-31 23:33:06 +0530

    Implemented error handling class for operational errors.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -22,5 +23,33 @@ app.use(express.static(`${__dirname}/public`)); // middleware for serving static
 app.use("/api/v1/tours", tourRouter);
 
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.url} on the server.`,
+  // });
+
+  // const err = new Error(`Can't find ${req.url} on the server.`);
+  // err.status = "fail";
+  // err.statusCode = 404;
+
+  const err = new AppError(`Can't find ${req.url} on the server.`, 404);
+
+  next(err); // is next receives an arguement express automatically knows that there has been an error.
+});
+
+app.use((err, req, res, next) => {
+  // Express recognises as error handling middleware function due to its arguements. (err function as arguement)
+
+  err.status = err.status || "error";
+  err.statusCode = err.statusCode || 500;
+
+  res
+    .status(err.statusCode)
+    .json({ status: err.statusCode, message: err.message });
+
+  next();
+});
 
 module.exports = app;
