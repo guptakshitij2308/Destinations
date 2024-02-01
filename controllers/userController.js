b81f6b2da43e4458e528e9762e8764e36240ed3e
Kshitij Gupta <guptakshitij2308@gmail.com>
2024-02-02 01:43:59 +0530

    Hidden config file and updated gitingore to stop tracking config files.

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ const catchAsync = require("../utils/catchAsync");
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
-  //
 
   res
     .status(200)
