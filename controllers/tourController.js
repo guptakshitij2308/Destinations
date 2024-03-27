eed637414836d2e2952b8fe1f629078db06a6a62
Kshitij <guptakshitij2308@gmail.com>
2024-03-27 17:03:15 +0530

    Implemented upload of tours photos using multer.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,65 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
+const multer = require("multer");
+const sharp = require("sharp");
 const Tour = require("../models/tourModel");
 const AppError = require("../utils/appError");
 
 const catchAsync = require("../utils/catchAsync");
 const factory = require("./handlerFactory");
+
+const multerStorage = multer.memoryStorage();
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Not an image! Please upload only images", 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: "imageCover", maxCount: 1 },
+  { name: "images", maxCount: 3 },
+]);
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // console.log(req.files.imageCover);
+
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, idx) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${idx + 1}.jpeg`;
+      console.log(filename);
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat("jpeg")
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    }),
+  );
+
+  next();
+});
+
+// upload.single : when we want to upload a single image
+// upload.array : when we want to upload multiple images // req.files
 
 // prefilling the query string for the user so that the user does not have to fill it.
 exports.aliasTopTours = (req, res, next) => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -34,19 +34,19 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single("photo");
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat("jpeg")
     .jpeg({ quality: 90 }) // save the image to memory instead of disk in case of ip
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 const filterObj = (obj, ...allowedFields) =>
   Object.keys(obj).reduce((newObj, key) => {
